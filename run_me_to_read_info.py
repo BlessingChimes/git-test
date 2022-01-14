1cd632477fd36775c1d07ff6c214fa951cf76ca8
= <794717561@qq.com>
2022-01-14 16:51:02 +0800

    创建分支并改动代码

--- run_me_to_read_info.py
+++ run_me_to_read_info.py
@@ -1,9 +1,15 @@
 def print_info():
     print('hello git')
     print('this repo is used for testing git')
+
+def print_change():
     print('this line is used to test git change file')
+
+def print_branch():
+    print('this is used for testing branch')
 
 
 if __name__=='__main__':
-    print_info()
-
+    # print_info()
+    # print_change()
+    print_branch()
